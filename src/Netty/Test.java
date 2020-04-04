f67ec30f167b0c6e1c934e818c4964360f8d34f4
sj <184500237@qq.com>
2020-04-05 00:12:01 +0800

    今日练习  连接池 今日讨论  HashMap LinkedBlockingQueue 今日未完成  leetcode   HikariCP和druid的效率还没测。。。
    未来几天的计划
    再写一遍聊天室，丰富内容
    
    每日写总结

--- src/Netty/Test.java
+++ src/Netty/Test.java
@@ -2,23 +2,30 @@ package Netty;
 
 //import com.sun.javafx.webkit.EventLoopImpl;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 
 public class Test {
     public static void main(String[] args) {
 //        EventLoopGroup
         EventLoopGroup bossGroup = new NioEventLoopGroup();
-//        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
 //        try{
 //            ServerBootstrap b = new ServerBootstrap();
 //            b.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class)
 //                    .childHandler(new ChannelInitializer() {
 //                        @Override
 //                        protected void initChannel(Channel ch) throws Exception {
-//                            ch.pipeline().addLast(new O)
+//                            ch.pipeline().addLast(new )
 //                        }
 //                    })
+//        }catch (Exception e){
+//            e.printStackTrace();
 //        }
     }
 }
