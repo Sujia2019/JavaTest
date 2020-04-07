fdafc136e4764ae003c4ea8db703c6e59c5badee
alienware <184500237@qq.com>
2020-04-07 15:15:16 +0800

    刷题笔记

---
+++ src/Algorithm/leetcode/ch0407.java
@@ -0,0 +1,12 @@
+package Algorithm.leetcode;
+
+public class ch0407 {
+
+
+    public static void main(String args[]) {
+//        int i = -5;
+//        i =  ++(i++);
+//        System.out.println(i);
+    }
+
+}
--- src/Netty/Test.java
+++ src/Netty/Test.java
@@ -2,13 +2,19 @@ package Netty;
 
 //import com.sun.javafx.webkit.EventLoopImpl;
 
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
+//import io.netty.channel.EventLoopGroup;
+//import io.netty.channel.nio.NioEventLoopGroup;
+
+import java.sql.Statement;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class Test {
+    public int x;
     public static void main(String[] args) {
 //        EventLoopGroup
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
+//        EventLoopGroup bossGroup = new NioEventLoopGroup();
 //        EventLoopGroup workerGroup = new NioEventLoopGroup();
 //        try{
 //            ServerBootstrap b = new ServerBootstrap();
@@ -20,5 +26,16 @@ public class Test {
 //                        }
 //                    })
 //        }
+//        System.out.println(x);
+
+//        for(int m = 0, n = -1; n = 0; m++,n++)
+//        Map
+//        List
+//        Iterator
+        byte b = 123;
+        float f = -123;
+        long x = 0123;
+        double d = 0x12345678;
     }
+
 }
