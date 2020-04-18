b9f22fca4e6e46546ca0841f2b134f9cb1fa8539
sj <184500237@qq.com>
2020-04-18 23:12:26 +0800

    将netty注入spring的练习和jdbc例子

---
+++ netty/src/main/java/netty/springnetty/NettyConfig.java
@@ -0,0 +1,11 @@
+package netty.springnetty;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"netty.springnetty"})
+public class NettyConfig {
+
+}
