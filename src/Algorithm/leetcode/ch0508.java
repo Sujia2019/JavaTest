26f1a45f6b210887f30709975b8a7a22fb2ea39a
alienware <184500237@qq.com>
2020-05-08 23:14:09 +0800

    leetcode

--- src/Algorithm/leetcode/ch0508.java
+++ src/Algorithm/leetcode/ch0508.java
@@ -2,9 +2,11 @@ package Algorithm.leetcode;
 //将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
 public class ch0508 {
 
-
+    //要注意链表的 指向
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        //记录头指针
         ListNode res = new ListNode(0);
+        //记录当前位置
         ListNode index = res;
         while(l1!=null&&l2!=null){
             if(l1.val<l2.val){
@@ -22,28 +24,8 @@ public class ch0508 {
         }else{
             index.next=l1;
         }
+        //因为第一个是自己创建的，所以返回的应该是next
         return res.next;
-        // 类似归并排序中的合并过程
-//        ListNode dummyHead = new ListNode(0);
-//        ListNode cur = dummyHead;
-//        while (l1 != null && l2 != null) {
-//            if (l1.val < l2.val) {
-//                cur.next = l1;
-//                cur = cur.next;
-//                l1 = l1.next;
-//            } else {
-//                cur.next = l2;
-//                cur = cur.next;
-//                l2 = l2.next;
-//            }
-//        }
-//        // 任一为空，直接连接另一条链表
-//        if (l1 == null) {
-//            cur.next = l2;
-//        } else {
-//            cur.next = l1;
-//        }
-//        return dummyHead.next;
     }
     static class ListNode {
         int val;
