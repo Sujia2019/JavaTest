2def2b378236ad1f7017110ab767c2d04292f006
sujia2020 <sujia@oschina.cn>
2020-09-15 17:16:33 +0800

    leetcode -simple binary search

---
+++ src/Algorithm/leetcode/ch0915.java
@@ -0,0 +1,33 @@
+package Algorithm.leetcode;
+
+/**
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * <p>
+ * 你可以假设数组中无重复元素。
+ */
+public class ch0915 {
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 3, 5, 6};
+        int target = 5;
+        int res = searchInsert(nums, target);
+        System.out.println(res);
+    }
+
+}
