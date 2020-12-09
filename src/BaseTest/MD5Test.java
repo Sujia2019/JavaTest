ae26f1979976782c24326841333e9a52b78ec86c
v_sujia <v_sujia@baidu.com>
2020-12-09 13:03:41 +0800

    threadlocal

---
+++ src/BaseTest/MD5Test.java
@@ -0,0 +1,22 @@
+package BaseTest;
+
+import org.springframework.util.DigestUtils;
+import sun.misc.BASE64Encoder;
+
+import java.util.Base64;
+
+/**
+ * @author ：sujia
+ * @date ：Created in 2020/12/7 8:55 下午
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class MD5Test {
+    public static void main(String[] args) {
+        DigestUtils.md5Digest("osc-admin".getBytes());
+        String base64Private = new BASE64Encoder().encode("osc-admin".getBytes());
+        System.out.println(base64Private);
+        System.out.println(DigestUtils.md5DigestAsHex(new BASE64Encoder().encode("osc-admin".getBytes()).getBytes()));
+    }
+}
--- src/BaseTest/ThreadTest.java
+++ src/BaseTest/ThreadTest.java
@@ -13,17 +13,8 @@ import java.util.concurrent.TimeUnit;
  */
 public class ThreadTest {
     public static void main(String[] args) {
-        ThreadPoolExecutor pool = new ThreadPoolExecutor(3, 5,
-                1, TimeUnit.SECONDS, new ArrayBlockingQueue<>(3));
-        pool.submit(new Test("1号"));
-        pool.submit(new Test("2号"));
-        pool.submit(new Test("3号"));
-        pool.submit(new Test("4号"));
-        pool.submit(new Test("5号"));
-        pool.submit(new Test("6号"));
-        pool.submit(new Test("7号"));
-        pool.submit(new Test("8号"));
-        pool.submit(new Test("9号"));
+//        threadPoolTest();
+        threadLocalTest();
     }
 
     static class Test implements Runnable {
@@ -42,6 +33,42 @@ public class ThreadTest {
                 e.printStackTrace();
             }
             System.out.println(name + "，睡后");
+        }
+    }
+
+    private static void threadPoolTest() {
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(3, 5,
+                1, TimeUnit.SECONDS, new ArrayBlockingQueue<>(3));
+        pool.submit(new Test("1号"));
+        pool.submit(new Test("2号"));
+        pool.submit(new Test("3号"));
+        pool.submit(new Test("4号"));
+        pool.submit(new Test("5号"));
+        pool.submit(new Test("6号"));
+        pool.submit(new Test("7号"));
+        pool.submit(new Test("8号"));
+        pool.submit(new Test("9号"));
+    }
+
+    private static void threadLocalTest() {
+        User user1 = new User();
+        user1.setName("test111");
+        ThreadLocal<User> threadLocal = new ThreadLocal<>();
+        threadLocal.set(user1);
+        User user2 = threadLocal.get();
+        user2.setName("222");
+        System.out.println(threadLocal.get().getName());
+    }
+
+    static class User {
+        String name;
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
         }
     }
 }
